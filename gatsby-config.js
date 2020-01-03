a210db04b3e8764aa0eacb2941069a16e8b531d9
Akshay M <akshay.milmile@gmail.com>
2020-01-03 20:52:22 +0100

    Configured gatsby fileystenm and transformation plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,5 +29,20 @@ module.exports = {
     },
     "gatsby-plugin-emotion",
     "gatsby-plugin-netlify-cms",
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/static/assets`,
+        name: "images",
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/content`,
+      },
+    },
+    "gatsby-transformer-remark",
   ],
 }
