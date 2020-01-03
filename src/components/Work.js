849b863df6f90111255db12832b6b1d107a347ef
Akshay M <akshay.milmile@gmail.com>
2020-01-03 22:06:37 +0100

    minor bug fixes

--- src/components/Work.js
+++ src/components/Work.js
@@ -18,9 +18,4 @@ export default ({ workDesc }) => {
   )
 }
 
-// I am currently working at my first company
-// <a href="https://www.tcs.com/">Tata Consultancy Services</a>. I can't
-// disclose the specifics of my work. Most of my work involves around
-// understanding client requirements and providing them with the solution
-// which works. I am also a part of small team which deals with bringing
-// innovation, rapid prototyping and engineering experiences.
+
